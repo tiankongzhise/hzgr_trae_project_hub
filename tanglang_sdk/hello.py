571faa8e2c238adaad8bc7d7765b693ee6af054c
hbc_thinkbook <419337244@qq.com>
2025-04-03 10:12:54 +0800

    跑通螳螂的基础调用

---
+++ tanglang_sdk/hello.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from tanglang-sdk!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ tanglang_sdk/test/test.py
@@ -0,0 +1,6 @@
+from src.tl_sdk.client import main
+
+
+
+if __name__ == '__main__':
+    main()
